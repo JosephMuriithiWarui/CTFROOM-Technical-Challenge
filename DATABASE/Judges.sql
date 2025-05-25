5713b0282bf27e78623718c33f4eb85ab6aeb9d5
JosephMuriithiWarui <110674813+JosephMuriithiWarui@users.noreply.github.com>
2025-05-25 15:14:41 +0300

    Judges.sql

--- DATABASE/Judges.sql
+++ DATABASE/Judges.sql
@@ -1,19 +1,17 @@
 <!-- DATABASE -->
 -- Create the database
-CREATE DATABASE judging_system;
+CREATE DATABASE judge;
 
 -- Select the database to use
-USE judging_system;
+USE judge;
 
 -- Create the judges table
-CREATE TABLE judges (
-    judgeId INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL
-);
+CREATE TABLE judges ( 
+    judgeId VARCHAR(50) PRIMARY KEY, 
+    username VARCHAR(100) NOT NULL, 
+    password VARCHAR(100) NOT NULL );
 
--- Create the users table
-CREATE TABLE users (
-    userId VARCHAR(100) PRIMARY KEY,
-    userName VARCHAR(100) NOT NULL
-);
+CREATE TABLE users ( 
+    userId VARCHAR(50) PRIMARY KEY, 
+    userName VARCHAR(100) NOT NULL, 
+    Score INT DEFAULT 0 );
